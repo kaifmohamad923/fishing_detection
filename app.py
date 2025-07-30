0b48713501da2bd81d753e93f0b30993b0f10793
kaifmohamad923 <kaifmohamad923@gmail.com>
2025-07-30 23:18:36 +0530

    some changes

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ model = joblib.load("model.pkl")
 @app.route('/', methods=['GET', 'POST'])
 def home():
     result = None
-    color = None  # Add color variable
-
+    color = None
+    shadow = None
     if request.method == 'POST':
         url = request.form['url']
         features = [extract_features(url)]
         prediction = model.predict(features)[0]
         result = "PHISHING" if prediction == 1 else "LEGITIMATE"
-        color = "red" if result == "PHISHING" else "green"
-
-    return render_template('index.html', result=result, color=color)
+        color = "red" if prediction == 1 else "lime"
+        shadow = "#FD0404" if prediction == 1 else "#00FF00"  # ✅ shadow changes too
+    return render_template('index.html', result=result, color=color, shadow=shadow)
 
 if __name__ == '__main__':
     app.run(debug=True)
