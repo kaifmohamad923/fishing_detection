18e19bf93eecc9d53ad545a74a0e8748cc8e2877
kaifmohamad923 <kaifmohamad923@gmail.com>
2025-07-30 23:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
         url = request.form['url']
         features = [extract_features(url)]
         prediction = model.predict(features)[0]
-        result = "PHISHING" if prediction == 1 else "LEGITIMATE"
+        result = "PHISHING ❌" if prediction == 1 else "VERIFIED ✅️"
         color = "red" if prediction == 1 else "lime"
         shadow = "#FD0404A0" if prediction == 1 else "#00FF00A4"  # ✅ shadow changes too
     return render_template('index.html', result=result, color=color, shadow=shadow)
